603a6595b074f91cf356c1601a6cb03800363c63
tishismile <99396501+tishismile@users.noreply.github.com>
2022-04-16 19:45:53 +0300

    Add files via upload

--- prime.py
+++ prime.py
@@ -1,12 +1,10 @@
 def is_prime(n):
-    if 1<=n<=3:
+    if 2<=n<=3:
         return True
-    elif n<=0:
+    elif n<=1:
         return False
     else:
         for x in range(2,n):
              if n%x==0:
                  return False
-             else:
-                return True
-    
+        return True
